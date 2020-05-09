1a148dbcb9e6959704bf6567af01d2b3fcec04e5
acheinu1 <acheinue@gmail.com>
2020-05-09 18:30:41 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function (){
+    //set options
+    var speed = 500;            //fade speed
+    var autoswitch = true;          //auto slider option
+    var autoswitch_speed = 4000;        //auto slider speed
+
+    //add class active to the first slide class
+    $(".slide") .first() .addClass("active");
+
+    //hide all the 5 slides
+    $(".slide") .hide();
+
+    // show only active slide
+    $(".active") .show();
+
+    //the next icon
+    $("#next") .on("click",function(){
+
+        //this is to set the previous page to old
+        $(".active") .removeClass(".active") .addClass("oldActive");
+        if($(".oldActive") .is(":last-child")){
+
+        // when the last(5) is out we want first(1) as the next slide
+        $(".slide") .first() .addClass("active");
+        }
+        else{
+        $(".oldActive").next().addClass("active");
+        }
+        $(".oldActive") .removeClass(".oldActive");
+        $(".slide").fadeOut(speed)
+        $(".active").fadeIn(speed)
+    })
+
+    // the previous button
+    $("#prev") .on("click",function(){
+        
+        $(".active") .removeClass(".active") .addClass("oldActive");
+        if($(".oldActive") .is(":first-child")){
+
+        
+        $(".slide") .last() .addClass("active");
+        }
+        else{
+        $(".oldActive").prev().addClass("active");
+        }
+        $(".oldActive") .removeClass(".oldActive");
+        $(".slide").fadeOut(speed)
+        $(".active").fadeIn(speed)
+    })
+})
